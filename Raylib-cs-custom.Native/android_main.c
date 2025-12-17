b04fc024de1bdfee392966a551f7ab91ec8347f9
Cherry <aazetthr@gmail.com>
2025-12-17 21:28:22 +0300

    chore: explcitly mark function as visible

--- Raylib-cs-custom.Native/android_main.c
+++ Raylib-cs-custom.Native/android_main.c
@@ -1,9 +1,11 @@
 ﻿#if defined(PLATFORM_ANDROID)
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef void (*rlAndroidMainCallback)();
 static rlAndroidMainCallback androidCallback = NULL;
 
+__attribute__((visibility("default"))) __attribute__((used))
 void rlSetAndroidMain(rlAndroidMainCallback callback) {
     androidCallback = callback;
 }
@@ -12,6 +14,7 @@ int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
     while (androidCallback == NULL) {
+        usleep(100000);
     }
     androidCallback();
     return 0;

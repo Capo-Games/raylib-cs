4bde76f06ce1b0b642a5ea819dbeee664f51ec5a
Cherry <aazetthr@gmail.com>
2025-12-17 17:58:27 +0300

    chore: use external file

---
+++ Raylib-cs.Native/android_main.c
@@ -0,0 +1,19 @@
+﻿#if defined(PLATFORM_ANDROID)
+#include <stdlib.h>
+
+typedef void (*rlAndroidMainCallback)();
+static rlAndroidMainCallback androidCallback = NULL;
+
+void rlSetAndroidMain(rlAndroidMainCallback callback) {
+    androidCallback = callback;
+}
+
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    while (androidCallback == NULL) {
+    }
+    androidCallback();
+    return 0;
+}
+#endif

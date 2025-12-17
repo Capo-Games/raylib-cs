afd81214d211fa8f091816c2b0064445bd08693f
Cherry <aazetthr@gmail.com>
2025-12-18 01:21:21 +0300

    chore: revert

--- Raylib-cs-custom.Native/android_main.c
+++ Raylib-cs-custom.Native/android_main.c
@@ -1,12 +1,22 @@
 ﻿#if defined(PLATFORM_ANDROID)
 #include <stdlib.h>
+#include <unistd.h>
 
-extern void RaylibAndroidEntry();
+typedef void (*rlAndroidMainCallback)();
+static rlAndroidMainCallback androidCallback = NULL;
+
+__attribute__((visibility("default"))) __attribute__((used))
+void rlSetAndroidMain(rlAndroidMainCallback callback) {
+    androidCallback = callback;
+}
 
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
-    RaylibAndroidEntry();
+    while (androidCallback == NULL) {
+        usleep(100000);
+    }
+    androidCallback();
     return 0;
 }
 #endif
